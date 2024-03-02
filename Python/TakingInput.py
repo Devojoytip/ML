8df45e858477d0e03a8bdf1d96ddfecd696c2a88
Devojoytip <paldevojoyti@gmail.com>
2024-03-03 00:20:13 +0530

    commit

---
+++ Python/TakingInput.py
@@ -0,0 +1,11 @@
+a=input()
+b=input("Enter b:")
+print(a)
+print(type(a)) # string
+print(b)
+
+a=int(input("Enter value of a: ")) # typecast string to int
+b=float(input("Enter value of b: ")) # typecast string to int
+
+print(a, type(a)) # string
+print(b, type(b))
